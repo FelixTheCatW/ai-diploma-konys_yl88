6a8b51e550c05e413dd795af2091da9f9ce30e5f
Ivan <i.s.konyshev@yandex.ru>
2026-04-25 00:03:32 +0500

    feat: add csv utils functions

--- src/main.py
+++ src/main.py
@@ -1,10 +1,6 @@
 from csv_utils import save_csv, load_csv, count_csv_rows, sum_column
 
-products = [
-    ["title", "price", "count"],
-    ["Ноутбук", 50000, 2],
-    ["Мышь", 1500, 5]
-]
+products = [["title", "price", "count"], ["Ноутбук", 50000, 2], ["Мышь", 1500, 5]]
 
 save_csv("products.csv", products)
 
